0d88c15db720d6b60cd51ffa62959f89102178be
Jasayasc <joelandressayas@gmail.com>
2024-01-19 11:58:49 -0500

    Esructuracion del proyecto y añadimos el template de adminlte

---
+++ config/global.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_PORT", 3306);
+define("DB_NAME", "dbsistema");
+
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+
+define("DB_ENCODE", "utf8");
+define("SYSTEM_NAME", "POS_SYSTEM");
+?>
